ec365c1f70d2a0bc221ce55f8b114b549d120174
Will Farrington <wcfarrington@gmail.com>
2015-10-19 16:44:42 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem "test-queue", github: "wfarr/test-queue", branch: "dot-test-queue"
+gem "rspec"
---
+++ spec/lib/example5_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+describe "#{__FILE__}" do
+  it "is truthy" do
+    sleep 1
+    expect(true).to equal(true)
+  end
+end
